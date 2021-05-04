f053f4eb9d4dd288ff2c9844a57534d5938ae554
Ariel Moise <ariel.m23@gmail.com>
2021-05-04 12:08:28 +0300

    socket.io

--- server/index.js
+++ server/index.js
@@ -1,24 +1,23 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const path = require("path");
 const app = express();
 
 // socket.io -
 const socketio = require("socket.io");
 const http = require("http");
-const server = http.createServer(app); // creating the server outside of the express library to later connect it to socket io
+const server = http.createServer(app);
 const io = socketio(server);
 
 //routers
-const usersRouter = require('./src/routers/user')
-const chatsRouter = require('./src/routers/chat')
+const usersRouter = require("./src/routers/user");
+const chatsRouter = require("./src/routers/chat");
 
-const publicDirectory = path.join(__dirname, '../client/build');
+const publicDirectory = path.join(__dirname, "../client/build");
 
-require('./src/db/mongoose')
+require("./src/db/mongoose");
 
-//! app.use(cors()) MUST be BEFORE using routers
 app.use(express.json());
 app.use(express.static(publicDirectory));
 app.use(cors());
@@ -27,19 +26,58 @@ app.use(chatsRouter);
 
 const PORT = process.env.PORT || 5000;
 
+const {
+  generateMessage,
+  generateLocationMessage,
+} = require("./src/utils/messages"); // messages utils
 
+io.on("connection", (socket) => {
+  console.log("New WebSocket connection");
 
-io.on('connection', socket => {
-    console.log('new webSocket connection');
-    
-    // socket on join
+  socket.on("join", (user, callback) => {
+    socket.join(user.room);
 
-    // message
+    socket.emit("message", generateMessage("Admin", "Welcome!"));
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        generateMessage("Admin", `${user.username} has joined!`)
+      );
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
 
-    // send location
+    callback();
+  });
 
-    // disconnect
-})
+  socket.on("sendMessage", ({ message, user }, callback) => {
+    io.to(user.room).emit("message", generateMessage(user.username, message));
+    callback();
+  });
 
+  socket.on("sendLocation", ({ coords, user }, callback) => {
+    io.to(user.room).emit(
+      "locationMessage",
+      generateLocationMessage(
+        user.username,
+        `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
+      )
+    );
+    callback();
+  });
 
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+  socket.on("disconnect", () => {
+    io.to(user.room).emit(
+      "message",
+      generateMessage("Admin", `${user.username} has left!`)
+    );
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+  });
+});
+
+server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
