19f8c62a3708b2287640b1e3c18fe910b6305d0e
Ariel Moise <ariel.m23@gmail.com>
2021-05-08 16:12:16 +0300

    Atlas

--- server/src/db/mongoose.js
+++ server/src/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+require('dotenv').config();
 
-mongoose.connect("mongodb://localhost:27017/wideChats", {
+mongoose.connect(process.env.MONGO, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
