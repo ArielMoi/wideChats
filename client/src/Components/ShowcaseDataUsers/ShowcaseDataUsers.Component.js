e3be3d185854d7d39a262c0bc50c9b2a6513b842
Ariel Moise <ariel.m23@gmail.com>
2021-05-18 12:10:49 +0300

    direct message

--- client/src/Components/ShowcaseDataUsers/ShowcaseDataUsers.Component.js
+++ client/src/Components/ShowcaseDataUsers/ShowcaseDataUsers.Component.js
@@ -5,7 +5,6 @@ const ShowcaseDataUsers = (data) => {
   return (
     <div>
       <h3>{data.name}</h3>
-      <button onClick={data.sendMessage}>sendMessage</button>
       {data.addFunc && <button onClick={data.addFunc}>{data.btnText}</button>}
       {data.showProfile && (
         <button onClick={data.showProfile}>Show Profile</button>
